5a968e555cea3f3bbc2ed91ccb4159016bccdce7
Mehdi682007 <123469029+Mehdi682007@users.noreply.github.com>
2025-01-14 11:52:14 +0330

    install-microtik.sh

---
+++ microtik.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ایجاد فایل اسکریپت microtik.sh
+cat << 'EOF' > microtik.sh
+#!/bin/bash
+wget https://download.mikrotik.com/routeros/7.16.2/chr-7.16.2.img.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=512 chr.img /mnt && \
+ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/ip service disable telnet
+/user set 0 name=root password=xxxxxx"
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=1024 of=/dev/vda && \
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Sleep 5 seconds" && \
+sleep 5 && \
+echo "Ok, reboot" && \
+echo b > /proc/sysrq-trigger
+EOF
+
+# تغییر مجوز اجرایی فایل
+chmod +x microtik.sh
+
+# اجرای فایل
+./microtik.sh
